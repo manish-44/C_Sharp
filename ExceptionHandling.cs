6f3f5e73204c7949369d026180af9357d503e62f
manish-44 <meena.44@iitj.ac.in>
2023-05-02 00:40:34 +0530

    added ExeptionHandling

---
+++ ExceptionHandling.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp1
+{
+    public class ExeptionHandling
+    {
+       
+       public static double divisionFunc(double x,double y)
+        {
+            if(y==0)
+            {
+                throw new System.DivideByZeroException();
+            }
+
+            return x/y;
+        } 
+
+        public void func()
+        {
+            //zero division handling
+            double num1 =20;
+            double num2 = 0;
+
+            try{
+                Console.WriteLine("20/0 ;{0}",divisionFunc(num1,num2));
+            }
+            catch(DivideByZeroException ex){
+                Console.WriteLine("You can''t divide by zero");
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine("An error occured");
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message );
+            }
+
+            finally {
+                Console.WriteLine("Cleaning Up");
+            }
+        }
+    }
+}
